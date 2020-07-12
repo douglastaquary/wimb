ef0c6857b23b0abe19c869ea0f5f60b3330ae109
Douglas Taquary <douglastaquary@gmail.com>
2020-07-12 20:47:51 -0300

    Initial Commit

---
+++ Shared/WhereIsMyBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereIsMyBusApp.swift
+//  Shared
+//
+//  Created by Douglas Taquary on 12/07/20.
+//
+
+import SwiftUI
+
+@main
+struct WhereIsMyBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
